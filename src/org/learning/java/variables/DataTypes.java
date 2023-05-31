71921d62a8bb44dcddf2f2381097069cd9100748
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2023-05-31 10:38:33 +0200

    data types and variables exercises

--- src/org/learning/java/variables/DataTypes.java
+++ src/org/learning/java/variables/DataTypes.java
@@ -19,6 +19,11 @@ public class DataTypes {
     int n1 = 5;
     int n2 = 10;
     System.out.println(n1 / n2);
+
+    double d = 0.1;
+    System.out.println(d + d + d + d + d + d + d + d);
+
+    System.out.println(Math.max(10.5, 15.9));
   }
 
 }
--- src/org/learning/java/variables/OperatingSystemSurvey.java
+++ src/org/learning/java/variables/OperatingSystemSurvey.java
@@ -1,12 +1,25 @@
 package org.learning.java.variables;
 
+import java.util.Scanner;
+
 public class OperatingSystemSurvey {
 
   public static void main(String[] args) {
+    // declare a Scanner
+    Scanner keyboardReader = new Scanner(System.in);
+
     // number of users per operating system
     int macUsers = 5;
     int windowsUsers = 20;
     int linuxUser = 1;
+
+    // override number of users from keyboard input
+    System.out.print("Insert Mac users: ");
+    macUsers = keyboardReader.nextInt();
+    System.out.print("Insert Windows users: ");
+    windowsUsers = keyboardReader.nextInt();
+    System.out.print("Insert Linux users: ");
+    linuxUser = keyboardReader.nextInt();
 
     // total users
     int totalUsers = macUsers + windowsUsers + linuxUser;
@@ -22,8 +35,30 @@ public class OperatingSystemSurvey {
     System.out.println("Percentage Mac: " + percMac);
     System.out.println("Percentage Windows: " + percWindows);
     System.out.println("Percentage Linux: " + percLinux);
+
+    // Which operating systems are most used
+  /*  double maxMacWindows = Math.max(percMac, percWindows);
+    double max = Math.max(percLinux, maxMacWindows);
+
+    System.out.println("Max percentage: " + max);*/
+
+    int maxUsers = Math.max(macUsers, Math.max(windowsUsers, linuxUser));
+    System.out.println("Max users: " + maxUsers);
+    String result = "The most used OS:";
+    if (maxUsers == macUsers) {
+      result += " Mac";
+    }
+    if (maxUsers == windowsUsers) {
+      result += " Windows";
+    }
+    if (maxUsers == linuxUser) {
+      result += " Linux";
+    }
+
+    System.out.println(result);
     
 
   }
+
 
 }
